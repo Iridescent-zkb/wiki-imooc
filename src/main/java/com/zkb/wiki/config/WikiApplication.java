38da7da80f86db123fb55d3aae3d27fc892a0069
kangbaozhu <62064846+kangbaozhu@users.noreply.github.com>
2021-06-15 14:01:12 +0800

    2-6 开发Hello World接口 访问地址 http://127.0.0.1:8880/hello

--- src/main/java/com/zkb/wiki/config/WikiApplication.java
+++ src/main/java/com/zkb/wiki/config/WikiApplication.java
@@ -1,13 +1,16 @@
-package com.zkb.wiki;
+package com.zkb.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-
-import static org.springframework.boot.Banner.Mode.LOG;
-
+/*
+扫描多个包的写法，json
+ @ComponentScan({"com.zkb","com.test"})
+ */
+@ComponentScan("com.zkb")
 @SpringBootApplication
 public class WikiApplication {
 
@@ -16,7 +19,7 @@ public class WikiApplication {
         SpringApplication app = new SpringApplication(WikiApplication.class);
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功！！");
-        LOG.info("地址：\thttp127.0.0.1：{}",env.getProperty("server.port"));
+        LOG.info("地址：\thttp://127.0.0.1：{}",env.getProperty("server.port"));
     }
 
 }
