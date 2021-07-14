9d3c7b1394a5f04c19ae12de596ef8d97569f0df
kangbaozhu <62064846+kangbaozhu@users.noreply.github.com>
2021-07-14 15:39:21 +0800

    5-4 列表查询接口支持动态SQL name参数有值是按name查，没值时查全部 封面小图样式调整

--- src/main/java/com/zkb/wiki/service/EbookService.java
+++ src/main/java/com/zkb/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.zkb.wiki.req.EbookReq;
 import com.zkb.wiki.resp.EbookResp;
 import com.zkb.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -20,7 +21,9 @@ public class EbookService {
     public List<EbookResp> list(EbookReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
+        if(!ObjectUtils.isEmpty(req.getName())){
         criteria.andNameLike("%"+req.getName()+"%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
 //        List<EbookResp> respList = new ArrayList<>();
