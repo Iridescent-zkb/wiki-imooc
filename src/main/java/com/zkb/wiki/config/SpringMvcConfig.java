d4b8ff80c208dab6a31be6cf7fcf02c6a0b0d19b
kangbaozhu <62064846+kangbaozhu@users.noreply.github.com>
2021-07-14 16:49:12 +0800

    5-8 增加拦截器，打印接口耗时

---
+++ src/main/java/com/zkb/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,25 @@
+package com.zkb.wiki.config;
+
+import com.zkb.wiki.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login");
+
+
+
+    }
+}
