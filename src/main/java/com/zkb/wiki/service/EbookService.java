b137d4f0650201aaf1d6547604ae9ec64a8c5cec
kangbaozhu <62064846+kangbaozhu@users.noreply.github.com>
2021-07-14 21:19:34 +0800

    6-4 集成PageHelper插件 将mapper日志级别改为trace,打印所有的sql日志

--- src/main/java/com/zkb/wiki/service/EbookService.java
+++ src/main/java/com/zkb/wiki/service/EbookService.java
@@ -1,30 +1,42 @@
 package com.zkb.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.zkb.wiki.domain.Ebook;
 import com.zkb.wiki.domain.EbookExample;
 import com.zkb.wiki.mapper.EbookMapper;
 import com.zkb.wiki.req.EbookReq;
 import com.zkb.wiki.resp.EbookResp;
 import com.zkb.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
 
+
 @Service
 public class EbookService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
 
     @Resource
     private EbookMapper ebookMapper;
 
     public List<EbookResp> list(EbookReq req){
+        PageHelper.startPage(1,3);
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if(!ObjectUtils.isEmpty(req.getName())){
         criteria.andNameLike("%"+req.getName()+"%");
         }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook>  pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}",pageInfo.getTotal());
+        LOG.info("总页数：{}",pageInfo.getPages());
 
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebookList ){
