c0945403a0496b5340c21067fa110a74aacdc8ac
kangbaozhu <62064846+kangbaozhu@users.noreply.github.com>
2021-07-17 14:59:54 +0800

    6-11 为保存接口增加参数校验

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -100,7 +100,7 @@
             const ebooks = ref();
             const pagination = ref({
                 current: 1,
-                pageSize: 1001,
+                pageSize: 10,
                 total: 0
             });
             const loading = ref(false);
@@ -194,16 +194,19 @@
                 modalLoading.value = true;
 
                 axios.post("/ebook/save", ebook.value).then((response) => {
+                    modalLoading.value = false;
                     const data = response.data;  //data = commonResp
                     if (data.success){
                         modalVisible.value = false;
-                        modalLoading.value = false;
+
 
                         //重新加载列表
                         handleQuery({
                             page:pagination.value.current,
                             size:pagination.value.pageSize
                         });
+                    }else {
+                        message.error(data.message);
                     }
 
                 });
