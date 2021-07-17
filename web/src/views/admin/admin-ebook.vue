05cf98c9dddf12edf38c6d3104bf72ddb8959a1c
kangbaozhu <62064846+kangbaozhu@users.noreply.github.com>
2021-07-17 21:13:18 +0800

    7-6 分类选择改为级联选择组件

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -86,14 +86,13 @@
             <a-form-item label="名称">
                 <a-input v-model:value="ebook.name" />
             </a-form-item>
-            <a-form-item label="分类一">
-                <a-input v-model:value="ebook.category1Id" />
+            <a-form-item label="分类">
+                <a-cascader
+                        v-model:value="categoryIds"
+                        :field-names="{ label: 'name', value: 'id', children: 'children' }"
+                        :options="level1"
+                />
             </a-form-item>
-            <a-form-item label="分类二">
-                <a-input v-model:value="ebook.category2Id" />
-            </a-form-item>
-
-
             <a-form-item label="描述">
                 <a-input v-model:value="ebook.description" type="textarea" />
             </a-form-item>
@@ -133,10 +132,12 @@
                 },
                 {
                     title: '分类一',
+                    key:'category1Id',
                     dataIndex: 'category1Id'
                 },
                 {
                     title: '分类二',
+                    key:'category2Id',
                     dataIndex: 'category2Id'
                 },
                 {
@@ -203,20 +204,19 @@
             /**
              * 数组，[100, 101]对应：前端开发 / Vue
              */
-            //const categoryIds = ref();
-             const ebook = ref({});
+            const categoryIds = ref();
+            const ebook = ref();
             const modalVisible = ref(false);
             const modalLoading = ref(false);
             const handleModalOk = () => {
                 modalLoading.value = true;
-
+                ebook.value.category1Id = categoryIds.value[0];
+                ebook.value.category2Id = categoryIds.value[1];
                 axios.post("/ebook/save", ebook.value).then((response) => {
                     modalLoading.value = false;
                     const data = response.data;  //data = commonResp
                     if (data.success){
                         modalVisible.value = false;
-
-
                         //重新加载列表
                         handleQuery({
                             page:pagination.value.current,
@@ -235,7 +235,7 @@
             const edit = (record:any) => {
                 modalVisible.value = true;
                 ebook.value = Tool.copy(record);
-                // categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
+                categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
             };
             //     ebook.value.category1Id = categoryIds.value[0];
             //     ebook.value.category2Id = categoryIds.value[1];
@@ -270,7 +270,38 @@
             };
 
 
+            const level1 =  ref();
+            //let categorys: any;
+            /**
+             * 查询所有分类
+             **/
+            const handleQueryCategory = () => {
+                loading.value = true;
+                axios.get("/category/all").then((response) => {
+                    loading.value = false;
+                    const data = response.data;
+                    if (data.success) {
+                        const categorys = data.content;
+                        console.log("原始数组：", categorys);
+
+                        level1.value = [];
+                        level1.value = Tool.array2Tree(categorys, 0);
+                        console.log("树形结构：", level1.value);
+
+                        // 加载完分类后，再加载电子书，否则如果分类树加载很慢，则电子书渲染会报错
+                        // handleQuery({
+                        //     page: 1,
+                        //     size: pagination.value.pageSize,
+                        // });
+                    } else {
+                        message.error(data.message);
+                    }
+                });
+            };
+
+
             onMounted(() => {
+                handleQueryCategory();
                 handleQuery({
                     page:1,
                     size:pagination.value.pageSize
@@ -298,6 +329,9 @@
                 modalVisible,
                 modalLoading,
                 handleModalOk,
+                categoryIds,
+                level1,
+
 
                 handleDelete
 
