a2152b57f5ca73ea17430dbe2ff8dc7fd8d67636
kangbaozhu <62064846+kangbaozhu@users.noreply.github.com>
2021-07-18 10:04:35 +0800

    7-8 点击二级分类时，显示该分类下的电子书

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -112,18 +112,32 @@ export default defineComponent({
     };
 
     const isShowWelcome = ref(true);
+    let categoryId2 = 0;
 
 
+      const handleQueryEbook = () =>{
+          axios.get("/ebook/list",{
+              params:{
+                  page: 1,
+                  size:1000,
+                  categoryId2: categoryId2
+              }
+          }).then((response)=>{
+              const data= response.data;
+              ebooks.value = data.content.list;
+              // ebooks1.books=data.content;
+          });
+      }
     const handleClick = ( value :any) => {
-      console.log("menu click",value)
-      // if (value.key === 'welcome') {
-      //   isShowWelcome.value = true;
-      // } else {
-      //   categoryId2 = value.key;
-      //   isShowWelcome.value = false;
-      //   handleQueryEbook();
-      // }
-      isShowWelcome.value = value.key === 'welcome';
+      // console.log("menu click",value)
+      if (value.key === 'welcome') {
+        isShowWelcome.value = true;
+      } else {
+        categoryId2 = value.key;
+        isShowWelcome.value = false;
+        handleQueryEbook();
+      }
+      // isShowWelcome.value = value.key === 'welcome';
     };
 
 
@@ -131,16 +145,7 @@ export default defineComponent({
 
     onMounted(()=> {
       handleQueryCategory();
-      axios.get("/ebook/list",{
-        params:{
-          page: 1,
-          size:1000
-        }
-      }).then((response)=>{
-        const data= response.data;
-        ebooks.value = data.content.list;
-        // ebooks1.books=data.content;
-      });
+
     });
 
     return{
