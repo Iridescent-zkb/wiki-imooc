338f2bcc63c1df0d8923b6053c504a42ad060e26
kangbaozhu <62064846+kangbaozhu@users.noreply.github.com>
2021-07-21 21:18:08 +0800

    11-5 增加定时任务，定时收集数据 要在xml里一次执行多个SQL的话，需要为数据库连接增加参数：allowMultiQueries=true

---
+++ src/main/java/com/zkb/wiki/service/EbookSnapshotService.java
@@ -0,0 +1,18 @@
+package com.zkb.wiki.service;
+
+import com.zkb.wiki.mapper.EbookSnapshotMapperCust;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class EbookSnapshotService {
+
+    @Resource
+    private EbookSnapshotMapperCust ebookSnapshotMapperCust;
+
+    public void genSnapshot() {
+        ebookSnapshotMapperCust.genSnapshot();
+    }
+
+}
