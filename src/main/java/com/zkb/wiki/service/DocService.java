3c97f744a28c9420beb568e6b34a9c1e3c4fbb7b
kangbaozhu <62064846+kangbaozhu@users.noreply.github.com>
2021-07-21 15:50:47 +0800

    10-6 增加定时器，定时执行电子书信息更新SQL

--- src/main/java/com/zkb/wiki/service/DocService.java
+++ src/main/java/com/zkb/wiki/service/DocService.java
@@ -150,5 +150,9 @@ public class DocService {
             throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
         }
     }
+
+    public void updateEbookInfo(){
+        docMapperCust.updateEbookInfo();
+    }
 }
 
