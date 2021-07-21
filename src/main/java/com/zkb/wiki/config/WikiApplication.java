06968a831b7bb0855c93c5229feba01d78ee5240
kangbaozhu <62064846+kangbaozhu@users.noreply.github.com>
2021-07-21 15:22:13 +0800

    10-5 定时任务示例

--- src/main/java/com/zkb/wiki/config/WikiApplication.java
+++ src/main/java/com/zkb/wiki/config/WikiApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /*
 扫描多个包的写法，json
  @ComponentScan({"com.zkb","com.test"})
@@ -14,6 +16,7 @@ import org.springframework.core.env.Environment;
 @ComponentScan("com.zkb")
 @SpringBootApplication
 @MapperScan("com.zkb.wiki.mapper")
+@EnableScheduling
 public class WikiApplication {
 
      private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
