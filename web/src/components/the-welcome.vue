b4a08ae2577078d1c7059a069dec5a66aca517a1
kangbaozhu <62064846+kangbaozhu@users.noreply.github.com>
2021-07-21 21:37:10 +0800

    11-7 将欢迎文字提取成组件

---
+++ web/src/components/the-welcome.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>欢迎使用小朱同学的wiki知识库123</h1>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'the-welcome',
+        setup () {
+
+
+            return {
+
+            }
+        }
+    });
+</script>
+
