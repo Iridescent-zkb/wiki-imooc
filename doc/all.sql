e5554e44c29cfc86767d6474389e3e3c8d825cff
kangbaozhu <62064846+kangbaozhu@users.noreply.github.com>
2021-07-21 20:14:45 +0800

    11-3 增加电子书快照表，生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -106,4 +106,17 @@ insert into `user` (id, `login_name`, `name`, `password`) values (1, 'test', '�
 
 
 
+-- 电子书快照表
+drop table if exists `ebook_snapshot`;
+create table `ebook_snapshot` (
+                                  `id` bigint auto_increment not null comment 'id',
+                                  `ebook_id` bigint not null default 0 comment '电子书id',
+                                  `date` date not null comment '快照日期',
+                                  `view_count` int not null default 0 comment '阅读数',
+                                  `vote_count` int not null default 0 comment '点赞数',
+                                  `view_increase` int not null default 0 comment '阅读增长',
+                                  `vote_increase` int not null default 0 comment '点赞增长',
+                                  primary key (`id`),
+                                  unique key `ebook_id_date_unique` (`ebook_id`, `date`)
+) engine=innodb default charset=utf8mb4 comment='电子书快照表';
 
