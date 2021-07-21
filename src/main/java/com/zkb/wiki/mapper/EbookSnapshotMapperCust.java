5a73d2031920f1374c4ed1781c69fb8449cb22fa
kangbaozhu <62064846+kangbaozhu@users.noreply.github.com>
2021-07-21 21:28:14 +0800

    11-7 后端增加获取统计数值接口

--- src/main/java/com/zkb/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/zkb/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,6 +1,12 @@
 package com.zkb.wiki.mapper;
 
+import com.zkb.wiki.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
 
     public void genSnapshot();
+
+    List<StatisticResp> getStatistic();
 }
