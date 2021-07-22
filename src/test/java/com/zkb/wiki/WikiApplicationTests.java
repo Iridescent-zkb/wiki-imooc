05098d260bd438533a94f3348446c0fbc8a6fb8c
kangbaozhu <62064846+kangbaozhu@users.noreply.github.com>
2021-07-22 16:36:11 +0800

    12-6 SpringBoot多环境匹配值，生产发布修改

--- src/test/java/com/zkb/wiki/WikiApplicationTests.java
+++ src/test/java/com/zkb/wiki/WikiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.zkb.wiki;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WikiApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.zkb.wiki;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WikiApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
