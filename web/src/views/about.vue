42b7a9de9f4e958d4675f72f6ebbf36436bbfa38
kangbaozhu <62064846+kangbaozhu@users.noreply.github.com>
2021-08-28 20:43:05 +0800

    修改了关于我们

--- web/src/views/about.vue
+++ web/src/views/about.vue
@@ -1,7 +1,7 @@
 <template>
   <a-layout>
       <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
-       <h1>本网站只是一个简单的SpringBoot+Vue的前后端分离的wiki知识库系统，欢迎收藏</h1>
+       <h1>本网站是SpringBoot+Vue的前后端分离的wiki知识库系统，测试账号：test 密码：test</h1>
         </a-layout-content>
      </a-layout>
 </template>
